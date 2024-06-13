c077c35401b5ce660c90aa7079bcd91547d112ce
Ramazan <sakenovramazan@gmail.com>
2024-06-13 23:39:49 +0500

    update

---
+++ topic-1-search/task2.js
@@ -0,0 +1,38 @@
+function binarySearch(arr, elem) {
+    let start = 0;
+    let end = arr.length-1;
+    let middle = Math.floor((start + end)/2);
+    console.log(start, end, middle);
+    
+    while (arr[middle] !== elem) {
+        if (elem < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+
+    if(arr[middle] === elem){
+        return middle;
+    }
+    return -1;
+}
+
+// Edge test cases
+binarySearch([1,2,3,4,5],2) // 1
+binarySearch([1,2,3,4,5],3) // 2
+binarySearch([1,2,3,4,5],5) // 4
+binarySearch([1,2,3,4,5],6) // -1
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 10) // 2
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 95) // 16
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 100) // -1
